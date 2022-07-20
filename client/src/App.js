995c1ad57008e62cbcd5809418efb50820d8f5f2
Matt Keys <MatthewjKeys22@gmail.com>
2022-07-20 13:05:07 -0600

    added dashboard page and some bootstrap elements just to start sandboxing. Going to start planning out logins, API, and general flow of the app. Be bac kt ot he code later. Paper for now.

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,40 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { StoreProvider } from "./utils/GlobalContext";
+import Dashboard from "./pages/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        Fuck you
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <StoreProvider>
+          <Switch>
+            <Route exact path="/" render={()=> <Dashboard />} />
+          </Switch>
+        </StoreProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
